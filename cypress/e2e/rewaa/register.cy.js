a810d6495814ae5afd4ea5afe3627d4fc63e843f
RewaH <78552917+RewaH@users.noreply.github.com>
2024-06-11 17:59:58 -0500

    4th change

--- cypress/e2e/rewaa/register.cy.js
+++ cypress/e2e/rewaa/register.cy.js
@@ -1,5 +1,5 @@
 ///<require='cypress'>
-describe("register test cases",()=>{
+describe("registeration test cases",()=>{
 
 it("register with valid data",()=>{
 cy.visit("https://guneet.pythonanywhere.com/register")
@@ -10,5 +10,49 @@ cy.xpath("//input[@value='Register']").click()
 cy.xpath("//p[contains(text(),'Registration successful. Please login.')]")
 })
 
+it("user is not able to register with wrong confirm password",()=>{
+    cy.visit("https://guneet.pythonanywhere.com/register")
+    cy.get("#username").type("Rewaa.Hamed")
+    cy.get("#password").type("Rewaa@12345")
+    cy.get("#confirm_password").type("Rewaa@1234567")
+    cy.xpath("//input[@value='Register']").click()
+    cy.xpath("//p[contains(text(),'Passwords do not match.')]")
+    })
 
+  it("user is not able to register with password less than 8 characters",()=>{
+    cy.visit("https://guneet.pythonanywhere.com/register")
+    cy.get("#username").type("Rewaa.Hamed")
+    cy.get("#password").type("Rew")
+    cy.get("#confirm_password").type("Rew")
+    cy.xpath("//input[@value='Register']").click()
+    cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+})  
+
+ it("user is not able to register with password with no uppercase letter",()=>{
+        cy.visit("https://guneet.pythonanywhere.com/register")
+        cy.get("#username").type("Rewaa.Hamed")
+        cy.get("#password").type("Rew")
+        cy.get("#confirm_password").type("Rew")
+        cy.xpath("//input[@value='Register']").click()
+        cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+    }) 
+
+ it("user is not able to register with password with no lowercase letter",()=>{            
+       cy.visit("https://guneet.pythonanywhere.com/register")
+       cy.get("#username").type("Rewaa.Hamed")
+       cy.get("#password").type("REW")
+       cy.get("#confirm_password").type("REW")
+       cy.xpath("//input[@value='Register']").click()
+       cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+    })       
+
+
+    it("user is not able to register with password with no digit",()=>{            
+        cy.visit("https://guneet.pythonanywhere.com/register")
+        cy.get("#username").type("Rewaa.Hamed")
+        cy.get("#password").type("rew")
+        cy.get("#confirm_password").type("rew")
+        cy.xpath("//input[@value='Register']").click()
+        cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+     })       
 })
