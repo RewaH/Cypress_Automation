d53faeb3c4a59a5b15215ae934bf49971a9205eb
RewaH <78552917+RewaH@users.noreply.github.com>
2024-06-20 11:05:01 -0500

    register & login Test cases

--- cypress/e2e/rewaa/login.cy.js
+++ cypress/e2e/rewaa/login.cy.js
@@ -1,50 +1,86 @@
 ///<require='cypress'>
-describe("RewaaFirstTestSuite",()=>{   //create test suite 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  });
+describe("Login Test Cases",()=> {   //create test suite 
+    let fd;
 
-it("user is able to login with right username & password",()=>{               //create test case
+    before(function() {     //hook to read data from external file
+        cy.fixture('login').then((data) => {
+          fd = data;
+        });
+      });
+
+it("user is able to login with right username & password",function(){               //create test case
 cy.visit("https://guneet.pythonanywhere.com/")
-cy.get("#username").type("Rewaa")
-cy.get("#password").type("Rewaa@123")
+
+fd.forEach(element => {   
+cy.get("#username").clear().type(element.Username)
+cy.get("#password").clear().type(element.Password)
 cy.contains("input","Login").click()
-cy.xpath("//a[contains(text(),'Home')]").click()
-cy.xpath("//a[contains(text(),'About')]").click()
+ cy.xpath("//a[contains(text(),'Logout')]").click()
 
-})
-it("user is not able to login with wrong username & right password",()=>{               //create test case
-    cy.visit("https://guneet.pythonanywhere.com/")
-    cy.get("#username").type("Rewaa123")
-    cy.get("#password").type("Rewaa@123")
-    cy.contains("input","Login").click()
-   // cy.xpath("//a[contains(text(),'Home')]").click()
-    //cy.xpath("//a[contains(text(),'About')]").click()
-    cy.get('[class="error"]').contains("Invalid username or password")
-    })
-    it("user is not able to login with right username & wrong password",()=>{               //create test case
+// cy.xpath("//a[contains(text(),'Home')]").click()
+// cy.xpath("//a[contains(text(),'About')]").click()
+
+});
+});
+
+ it("user is not able to login with wrong username & right password",function(){               //create test case
+     cy.visit("https://guneet.pythonanywhere.com/")
+
+     fd.forEach(element => {   
+cy.get("#username").clear().type(element.WrongUsername)
+cy.get("#password").clear().type(element.Password)
+cy.contains("input","Login").click()
+cy.get('[class="error"]').contains("Invalid username or password")
+
+});
+});
+    it("user is not able to login with right username & wrong password",function(){               //create test case
         cy.visit("https://guneet.pythonanywhere.com/")
-        cy.get("#username").type("Rewaa")
-        cy.get("#password").type("Rewaa@123456")
-        cy.contains("input","Login").click()
-       // cy.xpath("//a[contains(text(),'Home')]").click()
-        //cy.xpath("//a[contains(text(),'About')]").click()
-        cy.get('[class="error"]').contains("Invalid username or password")
-        })
-    it("user is not able to login with wrong username & wrong password",()=>{               //create test case
-            cy.visit("https://guneet.pythonanywhere.com/")
-            cy.get("#username").type("Rewaa123")
-            cy.get("#password").type("Rewaa@123456")
+        fd.forEach(element=> {
+            cy.get("#username").clear().type(element.Username)
+            cy.get("#password").clear().type(element.WrongPassword)
             cy.contains("input","Login").click()
-           // cy.xpath("//a[contains(text(),'Home')]").click()
-            //cy.xpath("//a[contains(text(),'About')]").click()
             cy.get('[class="error"]').contains("Invalid username or password")
-            })
-
-
-            it("user is not able to login with numeric username & numeric password",()=>{               //create test case
-                cy.visit("https://guneet.pythonanywhere.com/")
-                cy.get("#username").type("123@&%456")
-                cy.get("#password").type("456789@123%^&")
+            
+            });
+            });
+    it("user is not able to login with wrong username & wrong password",function(){               //create test case
+            cy.visit("https://guneet.pythonanywhere.com/")
+            fd.forEach(element=> {
+                cy.get("#username").clear().type(element.WrongUsername)
+                cy.get("#password").clear().type(element.WrongPassword)
                 cy.contains("input","Login").click()
                 cy.get('[class="error"]').contains("Invalid username or password")
-                })
                 
-})
+                });
+                });
+//             cy.get("#username").type("Rewaa123")
+//             cy.get("#password").type("Rewaa@123456")
+//             cy.contains("input","Login").click()
+//            // cy.xpath("//a[contains(text(),'Home')]").click()
+//             //cy.xpath("//a[contains(text(),'About')]").click()
+//             cy.get('[class="error"]').contains("Invalid username or password")
+//             })
+
+
+            it("user is not able to login with numeric username & numeric password",function(){               //create test case
+                cy.visit("https://guneet.pythonanywhere.com/")
+                fd.forEach(element=> {
+                    cy.get("#username").clear().type(element.NumericUsername)
+                    cy.get("#password").clear().type(element.NumericPassword)
+                    cy.contains("input","Login").click()
+                    cy.get('[class="error"]').contains("Invalid username or password")
+                    
+                    });
+                    });
+//                 cy.get("#username").type("123@&%456")
+//                 cy.get("#password").type("456789@123%^&")
+//                 cy.contains("input","Login").click()
+//                 cy.get('[class="error"]').contains("Invalid username or password")
+//                 })
+                
+// })
+});
--- cypress/e2e/rewaa/register.cy.js
+++ cypress/e2e/rewaa/register.cy.js
@@ -1,58 +1,95 @@
 ///<require='cypress'>
-describe("registeration test cases",()=>{
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  });
+  
+  describe('Registration Tese Cases', () => {
+    let fd;
+  
+    before(function() {
+      cy.fixture('register').then((data) => {
+        fd = data;
+      });
+    });
+  
+    it('register with valid data', function() {
+      cy.visit('https://guneet.pythonanywhere.com/register');
+     //  cy.wait(5000)
 
-it("register with valid data",()=>{
-cy.visit("https://guneet.pythonanywhere.com/register")
-cy.get("#username").type("Rewaa.Hamed")
-cy.get("#password").type("Rewaa@12345")
-cy.get("#confirm_password").type("Rewaa@12345")
-cy.xpath("//input[@value='Register']").click()
-cy.xpath("//p[contains(text(),'Registration successful. Please login.')]")
-})
+      fd.forEach(element => {
+        cy.get("#username").clear().type(element.Username);
+        cy.get("#password").clear().type(element.Password);
+        cy.get("#confirm_password").clear().type(element.ConfirmPassword);
 
-it("user is not able to register with wrong confirm password",()=>{
+        cy.get('input[type="submit"]').click();
+        cy.xpath("//p[contains(text(),'Registration successful. Please login.')]")
+       // cy.get("p[class='error']").contains("Invalid username or password.");
+      });
+    });
+
+
+    it('user is not able to register with wrong confirm password', function() {
+        cy.visit("https://guneet.pythonanywhere.com/register");
+        cy.wait(5000);
+
+        fd.forEach(element => {
+            cy.get("#username").clear().type(element.Username);
+            cy.get("#password").clear().type(element.Password);
+            cy.get("#confirm_password").clear().type(element.InvalidConfirmPassword);
+    
+            cy.get('input[type="submit"]').click();
+          //  cy.xpath("//p[contains(text(),'Registration successful. Please login.')]")
+            cy.get("p[class='error']").contains("Passwords do not match.");
+    });
+  });
+
+  it("user is not able to register with password less than 8 characters", function(){
     cy.visit("https://guneet.pythonanywhere.com/register")
-    cy.get("#username").type("Rewaa.Hamed")
-    cy.get("#password").type("Rewaa@12345")
-    cy.get("#confirm_password").type("Rewaa@1234567")
-    cy.xpath("//input[@value='Register']").click()
-    cy.xpath("//p[contains(text(),'Passwords do not match.')]")
-    })
+    fd.forEach(element => {
+        cy.get("#username").clear().type(element.Username);
+        cy.get("#password").clear().type(element.InvalidPasswordLessThan8Charecters);
+        cy.get("#confirm_password").clear().type(element.InvalidPasswordLessThan8Charecters);
 
-  it("user is not able to register with password less than 8 characters",()=>{
-    cy.visit("https://guneet.pythonanywhere.com/register")
-    cy.get("#username").type("Rewaa.Hamed")
-    cy.get("#password").type("Rew")
-    cy.get("#confirm_password").type("Rew")
-    cy.xpath("//input[@value='Register']").click()
-    cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
-})  
-
- it("user is not able to register with password with no uppercase letter",()=>{
-        cy.visit("https://guneet.pythonanywhere.com/register")
-        cy.get("#username").type("Rewaa.Hamed")
-        cy.get("#password").type("Rew")
-        cy.get("#confirm_password").type("Rew")
-        cy.xpath("//input[@value='Register']").click()
+        cy.get('input[type="submit"]').click();
         cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
-    }) 
+});
+});
 
- it("user is not able to register with password with no lowercase letter",()=>{            
+
+ it("user is not able to register with password with no uppercase letter",function(){
+        cy.visit("https://guneet.pythonanywhere.com/register")
+        fd.forEach(element => {
+            cy.get("#username").clear().type(element.Username);
+            cy.get("#password").clear().type(element.InvalidPasswordWithNoUppercaseLetter);
+            cy.get("#confirm_password").clear().type(element.InvalidPasswordWithNoUppercaseLetter);
+    
+            cy.get('input[type="submit"]').click();
+            cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+    });
+    });
+
+
+ it("user is not able to register with password with no lowercase letter",function(){            
        cy.visit("https://guneet.pythonanywhere.com/register")
-       cy.get("#username").type("Rewaa.Hamed")
-       cy.get("#password").type("REW")
-       cy.get("#confirm_password").type("REW")
-       cy.xpath("//input[@value='Register']").click()
-       cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
-    })       
+       fd.forEach(element => {
+        cy.get("#username").clear().type(element.Username);
+        cy.get("#password").clear().type(element.InvalidPasswordWithNoLowercaseLetter);
+        cy.get("#confirm_password").clear().type(element.InvalidPasswordWithNoLowercaseLetter);
 
-
-    it("user is not able to register with password with no digit",()=>{            
-        cy.visit("https://guneet.pythonanywhere.com/register")
-        cy.get("#username").type("Rewaa.Hamed")
-        cy.get("#password").type("rew")
-        cy.get("#confirm_password").type("rew")
-        cy.xpath("//input[@value='Register']").click()
+        cy.get('input[type="submit"]').click();
         cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
-     })       
-})
+});
+});      
+
+    it("user is not able to register with password with no digit",function(){            
+        cy.visit("https://guneet.pythonanywhere.com/register")
+        fd.forEach(element => {
+            cy.get("#username").clear().type(element.Username);
+            cy.get("#password").clear().type(element.InvalidPasswordWithNoDigit);
+            cy.get("#confirm_password").clear().type(element.InvalidPasswordWithNoDigit);
+    
+            cy.get('input[type="submit"]').click();
+            cy.get('[class="error"]').contains("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit")
+    });
+    });
+});
